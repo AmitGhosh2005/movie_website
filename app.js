4798b1129290a161eeb25328facef8d6d0409a88
Amit Ghosh <ghoshamit22826@gmail.com>
2025-05-02 00:30:30 +0530

    add all the files

---
+++ app.js
@@ -0,0 +1,27 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow,i)=>{
+    const itemNumber = movieLists[i].querySelectorAll("img").length;
+    let clickCounter = 0;
+    arrow.addEventListener("click",()=>{
+        clickCounter++;
+        if(itemNumber - (5 + clickCounter)>=0){
+            movieLists[i].style.transform = `translate(${movieLists[i].computedStyleMap().get("transform")[0].x.value-335}px)`
+        }else{
+            movieLists[i].style.transform = "translateX(0)";
+            clickCounter= 0;
+        }
+    });
+    console.log(movieLists[i].querySelectorAll(".img").length)
+});
+
+const ball  = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".content-container,.navbar-container,.left-menu-icon,.sidebar,.toggle");
+
+ball.addEventListener("click",()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active");
+    });
+    ball.classList.toggle("active");
+});
